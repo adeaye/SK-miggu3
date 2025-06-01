5c40f7b5e9bf2660d349b7b6f6ba1f3e355858e0
Ade armyatna <ade.armyatna.y@gmail.com>
2025-06-01 10:52:51 +0700

    Create script.js
    file javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+// Lengkapi kode supaya kode dapat berjalan, ganti ... dengan kode yang diperlukan.
+
+const nilaiUjian = [80, 50, 70, 80, 40];
+let totalNilai = 0;
+
+// lengkapi kondisi berhenti perulangan
+for (let i = 0; i < ... ; i++) {
+    // lengkapi rumus penambahan dengan nilai yang sedang diiterasi
+    totalNilai = totalNilai + ... ;
+}
+
+// lengkapi rumus
+const nilaiRatarata = totalNilai / ... ;
+
+// isi kondisi ketika nilai rata-rata 
+// lebih besar dari atau sama dengan 60 maka lulus
+if( ... ) {
+    console.log('Selamat Anda Lulus');
+} else {
+    console.log('Maaf Anda Tidak Lulus');
+}
